e4580789279bdb7f4baa67330a582a83adc81056
Ansh Nanda <anshnanda10@gmail.com>
2020-07-12 18:24:02 +0530

    Initial Commit

---
+++ Tiper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tiper
+//
+//  Created by Ansh Nanda on 7/12/20.
+//  Copyright © 2020 Ansh Nanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
