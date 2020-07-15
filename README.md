9d37092a2fdc57bd4b23dc3a7a0d65fd8cbbbf95
Ansh Nanda <anshnanda10@gmail.com>
2020-07-15 21:26:23 +0530

    Create basic app

---
+++ README.md
@@ -0,0 +1,11 @@
+#  Tip calculator app: iOS pre-word CodePath
+
+This pre-work incorporates the following 4 required steps and 2 optional steps:
+
+- [x] Setup Xcode
+- [x] Familiarize yourself with Swift
+- [x] Build the initial Tip Calculator
+- [ ] Optional: Add a Settings Screen to the Tip Calculator
+- [x] Optional: Extend your app, improve UI, add features
+    - [x] UI improved
+    - [x] Added locale 
--- Tiper/ViewController.swift
+++ Tiper/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var totalView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // setup
+        totalView.layer.cornerRadius = 10;
+        totalView.layer.masksToBounds = true;
+        tipPercentageLabel.text = "10%"
+        totalLabel.text = "0.00"
+        billAmountTextField.placeholder = "0.00"
+        billAmountTextField.keyboardType = UIKeyboardType.decimalPad
     }
-
-
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+        billAmountTextField.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentage = [0.15,0.18,0.2]
+        
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipPercentageLabel.text = String(format: "0%.2f", tip)
+        totalLabel.text = String(format: "0%.2f", total)
+    }
+    
+    @IBAction func onIncrement(_ sender: UIStepper) {
+        splitLabel.text = Int(sender.value).description
+    }
 }
 
